ee947b1349cb89f31a506d342ea71cac8b629827
Emmanuel Ocheme <emmanuelocheme86@gmail.com>
2025-07-11 14:34:49 +0000

    Check this out

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -7,7 +7,7 @@ import {genkitxOpenAI} from 'genkitx-openai';
 export const ai = genkit({
   plugins: [
     googleAI(),
-    genkitxOpenAI({apiKey: process.env.OPENAI_API_KEY}),
+    new genkitxOpenAI({apiKey: process.env.OPENAI_API_KEY}),
   ],
   logLevel: 'debug',
   model: 'googleai/gemini-2.0-flash',
